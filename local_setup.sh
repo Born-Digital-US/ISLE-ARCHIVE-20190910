b4058873fdbebd00cf74883872865de714a773c4
Noah Smith <noah.smith@commonmediainc.com>
2019-06-19 12:36:11 -0400

    rebuild with new paths

--- local_setup.sh
+++ local_setup.sh
@@ -19,21 +19,22 @@ docker exec -it isle-apache-ld bash -c "cd /var/www/html/sites/all/modules/islan
 docker exec -it isle-apache-ld bash -c "cd /var/www/html && drush en islandora_multi_importer -y"
 # say "IMI dependencies installed"
 
-git clone git@github.com:Born-Digital-US/isle-ingest-samples.git data/isle-apache-data/isle-ingest-samples
-docker exec -it isle-apache-ld bash -c "ln -s /var/www/html/isle-ingest-samples/behat /var/www/html/sites/behat && chown -R islandora:www-data /var/www/html/isle-ingest-samples/behat" # symlink up to the ingest samples location
-docker exec -it isle-apache-ld bash -c "mkdir /var/www/html/isle-ingest-samples/behat/debug/logs/"
+#git clone git@github.com:Born-Digital-US/isle-ingest-samples.git data/isle-apache-data/isle-ingest-samples
+git clone git@github.com:Born-Digital-US/isle-behat.git data/isle-apache-data/sites/behat
+#docker exec -it isle-apache-ld bash -c "ln -s /var/www/html/isle-ingest-samples/behat /var/www/html/sites/behat && chown -R islandora:www-data /var/www/html/isle-ingest-samples/behat" # symlink up to the ingest samples location
+docker exec -it isle-apache-ld bash -c "mkdir /var/www/html/sites/behat/debug/logs/"
 docker exec -it isle-apache-ld bash -c "cd /var/www/html/sites/behat && composer install"
 docker exec -it isle-apache-ld bash -c "cd /var/www/html && drush dis overlay -y"
 
-docker exec -it isle-apache-ld bash -c "cd /var/www/html && drush -u 1 islandora_batch_with_derivs_preprocess --key_datastream=MODS --scan_target=/var/www/html/isle-ingest-samples/Batches-by-CModel/Collections/files --use_pids=true --namespace=samples --parent=islandora:root --content_models=islandora:collectionCModel"
-docker exec -it isle-apache-ld bash -c "cd /var/www/html && drush -u 1 islandora_batch_ingest"
+# docker exec -it isle-apache-ld bash -c "cd /var/www/html && drush -u 1 islandora_batch_with_derivs_preprocess --key_datastream=MODS --scan_target=/var/www/html/isle-ingest-samples/Batches-by-CModel/Collections/files --use_pids=true --namespace=samples --parent=islandora:root --content_models=islandora:collectionCModel"
+# docker exec -it isle-apache-ld bash -c "cd /var/www/html && drush -u 1 islandora_batch_ingest"
 
 # docker exec -it isle-apache-ld bash -c "sh /var/www/html/isle-ingest-samples/Batches-by-CModel/ingest_samples.sh /var/www/html" # manually took the newspaper OCR stuff out
 # say "Samples ingested"
 
 # service tests first
 docker exec -it isle-apache-ld bash -c "cd /var/www/html/ && drush en simpletest -y"
-docker exec -it isle-apache-ld bash -c "cp /var/www/html/isle-ingest-samples/behat/test_config.ini /var/www/html/sites/all/modules/islandora/islandora/tests/"
-docker exec -it isle-apache-ld bash -c "chmod 775 /var/www/html/isle-ingest-samples/filter-drupal.xml"
+docker exec -it isle-apache-ld bash -c "cp /var/www/html/sites/behat/test_config.ini /var/www/html/sites/all/modules/islandora/islandora/tests/"
+docker exec -it isle-apache-ld bash -c "chmod 775 /var/www/html/sites/behat/filter-drupal.xml"
 docker exec -it isle-apache-ld bash -c "cd /var/www/html/sites/behat && ./run-isle-tests.sh --run=apache"
 # say "Testing complete"
--- local_teardown.sh
+++ local_teardown.sh
@@ -1,2 +1,3 @@
 docker-compose down -v
 rm -rf isle-ingest-samples && rm -rf data/isle-apache-data/isle-ingest-samples
+rm -rf isle-behat && rm -rf data/isle-apache-data/sites/bahat
